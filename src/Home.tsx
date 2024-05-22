14bf584feecd6cbd9417b2832f4aaf5d27c5194a
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-05-22 12:39:24 +0200

    testade att göra knappar

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './Home.css';
+import React, { useState } from 'react';
 
 function Home() {
+    
     const makeBtn = () => {
-        for(let i = 0; i < 3; i++){}
+        let buttons = [];
+        const labelBtn = ['UX', 'Front-End', 'Back-End'];
+
+        for(let i = 0; i < labelBtn.length; i++){
+            buttons.push(<button key={i} className={`btnClass ${labelBtn[i]}`}>{labelBtn[i]}</button>);
+        }return buttons;
     };
+
     
 
     return (
         <div>
-            <h2>Home</h2>
-            <p>Welcome to the Home page!</p>
+            <div className="btnDiv">{makeBtn()}</div>
+            <div className="taskDiv">TASKS HERE</div>
         </div>
     );
 }
