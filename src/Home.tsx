31062f78350d62dfb86c81a42e54d25665f7f50c
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-05-22 13:42:59 +0200

    börjae sätta upp firebase

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,13 +3,17 @@ import './Home.css';
 import React, { useState } from 'react';
 
 function Home() {
-    
+
     const makeBtn = () => {
         let buttons = [];
         const labelBtn = ['UX', 'Front-End', 'Back-End'];
 
         for(let i = 0; i < labelBtn.length; i++){
-            buttons.push(<button key={i} className={`btnClass ${labelBtn[i]}`}>{labelBtn[i]}</button>);
+            buttons.push(<button key={i}
+                className={`btnClass ${labelBtn[i]}`} 
+                //onClick={""}
+                >{labelBtn[i]}
+                </button>);
         }return buttons;
     };
 
